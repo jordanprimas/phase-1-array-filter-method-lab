4d3a0a5e6ae212cecce81a2bbd077b7dca838213
Jordan Primas <jorprimas20@gmail.com>
2023-03-02 00:27:19 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(array, string) {
+    const result = array.filter (name => name.toLowerCase() === string.toLowerCase())
+    return result 
+}
+
+function fuzzyMatch(array, string) {
+    const letters = array.filter (driver => driver.startsWith(string))
+    return letters
+}
+
+function matchName(array, string) {
+    const results = array.filter (driverName => driverName.name === string)
+    return results
+}
